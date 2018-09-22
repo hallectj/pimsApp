7a662b0e3ea42b6f6bd5b4ae7d3b2550c30b7085
Travis Halleck <hallectj@gmail.com>
2018-09-22 18:57:54 -0500

    Login screen comming together

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  #need logic here to render based off privelages or roles, check if user
+  
 end
