9ee3722e371232d18f1645c4f86f7c3b292d5b99
Tom Wood <tpw0007@uah.edu>
2018-10-25 17:50:20 -0500

    update controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -41,7 +41,7 @@ class PagesController < ApplicationController
       @patient = Patient.new(patient_params) 
     
       if @patient.save
-        render redirect_to root_path
+        render 'show'
       else
         render 'new_patient'
       end
@@ -154,7 +154,7 @@ private
   end  
 
   def patient_params
-    params.require(:patient).permit(:id, :first_name, :middle_name, :last_name, :search, admittance_attributes: [:date, :time, :reason], treatment_attributes: [:name, schedules_attributes: [:date, :time, :schedule_msg], prescriptions_attributes: [:name, :amount, :schedule], dr_notes_attributes: [:name, :message], n_notes_attributes: [:name, :message]], discharge_attributes: [:date, :time, bill_attributes: [:amount_paid, :amount_owed, :amount_insurance, charges_attributes: [:charge_name, :charge_amount]]])
+    params.require(:patient).permit(:id, :first_name, :middle_name, :last_name, :birthday, :search, admittance_attributes: [:date, :time, :reason], treatment_attributes: [:name, schedules_attributes: [:date, :time, :schedule_msg], prescriptions_attributes: [:name, :amount, :schedule], dr_notes_attributes: [:name, :message], n_notes_attributes: [:name, :message]], discharge_attributes: [:date, :time, bill_attributes: [:amount_paid, :amount_owed, :amount_insurance, charges_attributes: [:charge_name, :charge_amount]]])
   end
 
   #def patient_params
