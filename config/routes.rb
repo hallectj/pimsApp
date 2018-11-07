87f2c0c23909d80b68b0938d84c50ffbec84e4a5
Tom Wood <tpw0007@uah.edu>
2018-11-06 18:06:49 -0600

    update added editing charges

--- config/routes.rb
+++ config/routes.rb
@@ -90,8 +90,8 @@ Rails.application.routes.draw do
 
 
 
-    get '/pages/:discharge/bill/:bill_id/edit_charge/:charge_id', to: 'pages#edit_charge', as: :edit_charge
-    match "/pages/:discharge/bill/:bill_id/update_charge/:charge_id" => "pages#update_charge", as: :update_charge, via: [:patch, :post]
+    get 'pages/:patient_id/bill/:bill_id/edit_charge/:charge_id', to: 'pages#edit_charge', as: :edit_charge
+    match "pages/:patient_id/bill/:bill_id/update_charge/:charge_id" => "pages#update_charge", as: :update_charge, via: [:patch, :post]
         
     #end of nested edits and updates
 
